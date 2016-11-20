4204b596de5b30b1dd32cf4669a065c85973601a
PatrickSison <pmsison@hotmail.com>
2016-11-20 13:29:13 -0500

    Starts contact activity
    need to fix up contact activity before pushing. pushing this now so no
    conflicts when pushing mainactivity

--- CallYourMom/app/src/main/java/cmsc436/com/callyourmom/MainActivity.java
+++ CallYourMom/app/src/main/java/cmsc436/com/callyourmom/MainActivity.java
@@ -51,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent contactIntent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(contactIntent);
             }
         });
 
