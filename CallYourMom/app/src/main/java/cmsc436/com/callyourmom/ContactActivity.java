ad17aac2fa1edae94139ee385aee77e055968fce
PatrickSison <pmsison@hotmail.com>
2016-11-29 09:49:13 -0500

    Added phone number  selected

--- CallYourMom/app/src/main/java/cmsc436/com/callyourmom/ContactActivity.java
+++ CallYourMom/app/src/main/java/cmsc436/com/callyourmom/ContactActivity.java
@@ -1,9 +1,11 @@
 package cmsc436.com.callyourmom;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,6 +25,7 @@ import android.widget.*;
 import android.view.View.*;
 import android.net.*;
 import android.database.Cursor;
+
 import java.io.File;
 
 import org.json.JSONException;
@@ -35,18 +38,18 @@ import java.util.Random;
 
 public class ContactActivity extends AppCompatActivity {
 
-    private static TextView contactName;
+    private static TextView contactName, contactNumber;
     private NumberPicker np;
     private boolean contactPicked;
-    static final int PICK_CONTACT=1;
+    static final int PICK_CONTACT = 1;
     private static final String reminders = "remindersFile";
 
-
+    private String ayy = ContactActivity.class.getSimpleName();
     private String dataString;
     private SharedPreferences data;
     private SharedPreferences.Editor editor;
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
@@ -58,30 +61,37 @@ public class ContactActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         contactName = (TextView) findViewById(R.id.contact_name);
+        contactNumber = (TextView) findViewById(R.id.contact_number);
         np = (NumberPicker) findViewById(R.id.numberPicker);
         np.setMinValue(1);
         np.setMaxValue(365);
 
-        data = getSharedPreferences(reminders, 0);
-        dataString = data.toString();
+        data = getSharedPreferences("data", 0);
+        editor = data.edit();
+        dataString = data.getString(reminders, "0");
 
         final Button mSelectContact = (Button) findViewById(R.id.chooseContact);
-        mSelectContact.setOnClickListener(new OnClickListener(){
+        mSelectContact.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
                 startActivityForResult(intent, PICK_CONTACT);
             }
         });
 
         final Button mSubmit = (Button) findViewById(R.id.submit);
-        mSubmit.setOnClickListener(new OnClickListener(){
+        mSubmit.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 if (contactPicked) {
                     CallReminder newReminder = new CallReminder(contactName.getText().toString(), "3");
                     newReminder.setNumDaysForRemind(np.getValue());
-
+                    try {
+                        updateReminder(contactName.getText().toString(), "410-999-5555", 3);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    editor.putString(reminders, dataString);
                     finish();
                 } else {
                     Snackbar.make(v, "No Contact Selected", Snackbar.LENGTH_LONG).setAction("Action", null).show();
@@ -92,22 +102,24 @@ public class ContactActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
             Uri contactData = data.getData();
             Cursor c = managedQuery(contactData, null, null, null, null);
-            if (c.moveToFirst()){
+            if (c.moveToFirst()) {
                 String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                contactName.setText(name);
+                contactName.setText(name + " " + id);
                 contactPicked = true;
+                String number = c.getString(c.getColumnIndexOrThrow(Contacts.People.NUMBER));
+
             }
         }
     }
 
 
-    protected boolean updateReminder(String contactName) throws JSONException{
-        //Key: numDays    (JSONObject
+    protected void updateReminder(String contactName, String phoneNumber, int numDays) throws JSONException {
+        //Key: numDays      JSONObject
         //Val: contact
 
         //For contact       JSONArray
@@ -115,13 +127,23 @@ public class ContactActivity extends AppCompatActivity {
         //      Phone number
 
         JSONObject json;
-
-        if (dataString != null){
+        JSONArray group;
+        if (dataString != null)
             json = new JSONObject(dataString);
-            return true;
-        } else {
-            return false;
-        }
+        else
+            json = new JSONObject();
+
+        if (json.has(Integer.toString(numDays)))
+            json.put(Integer.toString(numDays), new JSONArray());
+        group = json.getJSONArray(Integer.toString(numDays));
+        JSONObject contact = new JSONObject();
+        contact.put("name", contactName);
+        contact.put("number", phoneNumber);
+        group.put(contact);
+        json.put(Integer.toString(numDays), group);
+        dataString = json.toString();
+
+        Log.e(ayy, "TESTING ----" + dataString);
 
     }
 
